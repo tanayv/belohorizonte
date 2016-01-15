e490350f07503bc23bc4eab3415e9be2e5f3bd49
Tanay Vardhan <tvardha2@illinois.edu>
2016-01-16 02:51:49 +0530

    Functional Commit 1
    - First Name one variant based customization
    - UI Complete with Material Design based form elements

--- script1.js
+++ script1.js
@@ -10,9 +10,28 @@ var ViewModel = function(first, last) {
 
 function CmpFirst(first) {
 //Break First Name
-	first = first.substr(0, 3);
+	
+	function isVowel(c) {
+    if (c == "a" || c == "e" || c == "i" || c == "o" || c == "u")
+    {
+    	return 1;
+    }
+}
+	
+	first = first.substr(0,3);
+	
+	if(isVowel(first.charAt(2)) == 1) {
+		var conson = ["r", "n", "b"];
+		s = Math.round(Math.random() * (2));
+		first = first +  conson[s];
+	}
+
+	var suffix = ["inho", "iano", "uel", "ardo", "andro", "enzo", "urilo", "andro", "enato"];
+	l = suffix.length;
+	s = Math.round(Math.random() * (l-1));
+		first = first + suffix[s];
 	return first;
 }
 
  
-ko.applyBindings(new ViewModel("Planet", "Earth")); // This makes Knockout get to work
+ko.applyBindings(new ViewModel("First", "Escobar")); // This makes Knockout get to work
